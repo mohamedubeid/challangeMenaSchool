46b8b7a61f5a89596f59f84e0fa9624c7a21fa53
M99ub <abozead112@outlook.com>
2021-07-10 13:44:22 +0300

    trying to solve ex1

--- ex1.js
+++ ex1.js
@@ -9,8 +9,21 @@ exp
  */
 
 function reverseNestedArray(array) {
-    //TODO
-}
+    for(let i = 0; i < array.length; i++) {
+        if(array[i].length > 1) {
+            reverseNestedArray(array[i]);
+        }else {
+            return array.reverse();
+        }
+    };
+    
 
+}
+a = [9,[5,[10,30]],6];
+
+//console.log(a.indexOf([ 2,3]));
+
+console.log(reverseNestedArray(a));
 
 module.exports = reverseNestedArray;
+
